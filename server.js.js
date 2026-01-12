022f5cfd234a3d33864dafca22837a7087642892
u1824520462-rgb <u1824520462@gmail.com>
2026-01-12 16:07:55 +0100

    server.js

--- server.js.js
+++ server.js.js
@@ -7,31 +7,43 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Serve i file statici dalla cartella corrente
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Gestione connessioni Socket.io
 io.on('connection', (socket) => {
     console.log('Un guerriero si è connesso:', socket.id);
 
-    socket.on('move', (data) => {
-        socket.broadcast.emit('enemyMove', data);
+    // Gestione dell'entrata nella stanza (Room)
+    socket.on('joinRoom', (roomCode) => {
+        socket.join(roomCode);
+        socket.currentRoom = roomCode; // Salviamo la stanza in cui si trova il socket
+        console.log(`Guerriero ${socket.id} entrato nella stanza: ${roomCode}`);
     });
 
-    socket.on('attack', () => {
-        socket.broadcast.emit('enemyAttack');
+    // Movimento: invia i dati solo agli altri nella stessa stanza
+    socket.on('move', (data) => {
+        if (socket.currentRoom) {
+            socket.to(socket.currentRoom).emit('enemyMove', data);
+        }
+    });
+
+    // Attacco: invia i dati dell'attaccante (posizione x, y) per calcolare il danno
+    socket.on('attack', (data) => {
+        if (socket.currentRoom) {
+            // Inviamo la posizione dell'attaccante all'avversario
+            socket.to(socket.currentRoom).emit('enemyAttack', data);
+        }
     });
 
     socket.on('disconnect', () => {
-        console.log('Guerriero disconnesso');
+        console.log('Guerriero disconnesso:', socket.id);
     });
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server attivo! Apri il browser su http://localhost:${PORT}`);
+    console.log(`Server attivo! Gioca su http://localhost:${PORT}`);
 });
